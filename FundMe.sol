6e4f5aef977fd76634c8369992935c164b6063b8
0x7c4e <iamgreatdiro@gmail.com>
2024-12-31 09:58:33 +0100

    add receive and fallback

--- FundMe.sol
+++ FundMe.sol
@@ -3,22 +3,26 @@ pragma solidity 0.8.26;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
+// before optimization, 783,444 gas
+// after - 739,900
 contract FundMe {
     using PriceConverter for uint256;
 
     // Minimum funding amount in USD, expressed in whole dollars
     // since the getconversionrate func returns amount in 18 decimal place, our mimunumusd value also has to be in 18 decimal place
     // here's 5 dollar in 18 dcimal place, can also be written as 5 * 1e18;
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     // create an array of addresses that fund us, to keep track
     address[] public funders;
 
-    address public owner;
+    address public immutable i_owner;
 
     // create a constructor to assign an owner to this contract (so that only owners can wwithdraw funds)
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     // create a map to see how much each funder has sent
@@ -26,10 +30,10 @@ contract FundMe {
 
     // Allow users to send ETH to this contract
     function fund() public payable {
-        // Require that the amount being sent is at least the equivalent of the minimumUsd in ETH
+        // Require that the amount being sent is at least the equivalent of the MINIMUM_USD in ETH
         // Note: The value of `msg.value` is in wei (1 ETH = 1e18 wei)
         // we use the getconversionrate func here to convert msg.value to usd
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH."); 
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH."); 
         // update the funders array whenever someone sends us funds
         funders.push(msg.sender);
 
@@ -59,7 +63,22 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "sender is not owner");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
         _;
     }
+
+    receive() external payable {
+        fund();
+     }
+
+     fallback() external payable {
+        fund();
+      }
 }
+
+
+
+// DEPLOYED CONTRACT ADDRESS
+// 0xBd354A770683FE4edf10392b3A71aD5a5D7B40dE
